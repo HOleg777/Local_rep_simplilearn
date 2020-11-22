7766f3da9361996d3650ff8ad8aea265087ee87e
Oleg_lenovo <o.havrun@gmail.com>
2020-11-22 15:08:39 -0500

    day3

---
+++ src/JavaPack/forExample.java
@@ -0,0 +1,25 @@
+package JavaPack;
+
+public class forExample {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+		  if(i==5)
+			  continue;
+		  System.out.println("i = " + i);
+		}
+
+		for(int i=2;i<=10;i++)
+		{
+		  if(i==5)
+			  break;
+		  System.out.println("i = " + i);
+		}
+		
+		
+		
+		
+	}
+
+}
